85a0e9fc1bdd833905bb396565b23bee36fd1707
Anderson César <andersonlscesar@gmail.com>
2023-05-20 18:18:33 -0300

    First Commit

---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,15 @@
+<header id="header">
+    <div class="container">
+        {{-- Navbar --}}
+        <nav class="nav">
+            <h1 class="nav__brand">NCC</h1>
+            {{-- Search form --}}
+            <form action="" method="GET" class="nav__form">
+                <input type="text" placeholder="Buscar por usuário ou postagem ..." class="input">
+                <button type="submit" class="btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+            {{-- end Search form --}}
+        </nav>
+        {{-- end navbar --}}
+    </div>
+</header>
