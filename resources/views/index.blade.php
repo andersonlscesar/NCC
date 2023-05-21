4283f823782197f2746937e3aa4108478e22f77d
Anderson César <andersonlscesar@gmail.com>
2023-05-21 08:36:51 -0300

    profile
    A grid was added in the main element and a little area to visualize the profile image was added too

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,5 +4,29 @@
 
 @section( 'content' )
 
+<div class="grid container">
+    {{-- Profile info --}}
+    <div class="profile">
+        <img src="{{ asset('img/profile.jpg') }}" alt="profile" class="profile__img">
+        <h2 class="profile__name">Naruto Uzumaki</h2>
+        <a href="#" class="link">Ver perfil</a>
+        <a href="#" class="link">Sair</a>
+    </div>
+    {{-- end Profile info --}}
+
+    {{-- Posts --}}
+    <div class="posts">
+
+    </div>
+    {{-- end Posts --}}
+
+    {{-- News --}}
+    <aside class="news">
+
+    </aside>
+    {{-- end News --}}
+
+
+</div>
 
 @endsection
